d8b749b5a38d97e87440149faf65b7f7eec05400
asalybek <luxuryteenager@gmail.com>
2020-03-26 22:37:58 +0600

    week 9

---
+++ Week9/shop_back/api/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'category_id': self.category_id_id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'count': self.count
+        }
+
+
+
